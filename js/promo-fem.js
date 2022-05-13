1b1ec159108ec434e55de10188737232dcc28e61
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-05-13 18:52:58 -0300

    Ajustes

--- js/promo-fem.js
+++ js/promo-fem.js
@@ -1,8 +1,8 @@
 const card1PromoFem = () => {
-    const imgPequenas = document.querySelectorAll(".js-img-peq-card1-fem");
+    const imgPequenas = document.querySelectorAll(".imgs__peq__prod .js-img-peq-card1-fem");
     imgPequenas.forEach((img) => {
         img.addEventListener("click", (e) => {
-            e.stopPropagation()
+            e.stopPropagation();
             const imgGrande = document.querySelector(".js-img-gra-card1-fem");
             imgGrande.src = img.src;
         });
@@ -16,25 +16,22 @@ const card2PromoFem = () => {
     const imgPequenas = document.querySelectorAll(".js-img-peq-card2-fem");
     imgPequenas.forEach((img) => {
         img.addEventListener("click", (e) => {
-            e.stopPropagation()
+            e.stopPropagation();
             const imgGrande = document.querySelector(".js-img-gra-card2-fem");
             imgGrande.src = img.src;
         });
     });
-
 }
 
 const card3PromoFem = () => {
     const imgPequenas = document.querySelectorAll(".js-img-peq-card3-fem");
     imgPequenas.forEach((img) => {
         img.addEventListener("click", (e) => {
-            e.stopPropagation()
+            e.stopPropagation();
             const imgGrande = document.querySelector(".js-img-gra-card3-fem");
             imgGrande.src = img.src;
         });
     });
-
-    console.log(imgPequenas)
 
 }
 
