a81607dd421b57be03727ab030b54e2219e801fb
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-05-13 18:07:07 -0300

    Ajustes

--- js/promo-fem.js
+++ js/promo-fem.js
@@ -1,7 +1,8 @@
 const card1PromoFem = () => {
     const imgPequenas = document.querySelectorAll(".js-img-peq-card1-fem");
     imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
+        img.addEventListener("click", (e) => {
+            e.stopPropagation()
             const imgGrande = document.querySelector(".js-img-gra-card1-fem");
             imgGrande.src = img.src;
         });
@@ -14,8 +15,21 @@ const card1PromoFem = () => {
 const card2PromoFem = () => {
     const imgPequenas = document.querySelectorAll(".js-img-peq-card2-fem");
     imgPequenas.forEach((img) => {
-        img.addEventListener("click", () => {
+        img.addEventListener("click", (e) => {
+            e.stopPropagation()
             const imgGrande = document.querySelector(".js-img-gra-card2-fem");
+            imgGrande.src = img.src;
+        });
+    });
+
+}
+
+const card3PromoFem = () => {
+    const imgPequenas = document.querySelectorAll(".js-img-peq-card3-fem");
+    imgPequenas.forEach((img) => {
+        img.addEventListener("click", (e) => {
+            e.stopPropagation()
+            const imgGrande = document.querySelector(".js-img-gra-card3-fem");
             imgGrande.src = img.src;
         });
     });
@@ -26,3 +40,4 @@ const card2PromoFem = () => {
 
 card1PromoFem();
 card2PromoFem();
+card3PromoFem();
